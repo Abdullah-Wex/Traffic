6f221f388d855ca8d1a3ad1285b39bd01f69123d
Abdullah-Wex <86279127+Abdullah-Wex@users.noreply.github.com>
2022-10-14 20:44:23 +0300

    Hope

--- main.py
+++ main.py
@@ -51,13 +51,13 @@ for option in options:
     
 driver = webdriver.Chrome(options = chrome_options)
 
-# params = {
-#     "latitude": 21.427526,
-#     "longitude": 39.271313,
-#     "accuracy": 100
-# }
+#params = {
+#    "latitude": 21.427526,
+#    "longitude": 39.271313,
+#    "accuracy": 100
+#}
 
-# driver.execute_cdp_cmd("Page.setGeolocationOverride", params)
+#driver.execute_cdp_cmd("Page.setGeolocationOverride", params)
 
 #options = Options()
 # options.add_argument("--window-size=1920,1200")
@@ -71,14 +71,52 @@ driver = webdriver.Chrome(options = chrome_options)
 # chrome_options = Options()
 # chrome_options.add_experimental_option("detach", True)
 
-driver.set_page_load_timeout(5000)
-driver.get("https://www.google.com/maps/@21.5495652,39.1973127,11.65z/data=!5m1!1e1")
-# driver.get("https://www.gps-coordinates.net/my-location")
-time.sleep(3)
-with open("page_source.html", "w") as f:
-  f.write(driver.page_source)
+for i in range(0,5):
+  date = datetime.today()
+  folder = r' '.join(['-'.join([str(date.year),str(date.month),str(date.day)])])
+  folder
+  driver.set_page_load_timeout(5000)
+  driver.get("https://www.google.com/maps/@21.5495652,39.1973127,11.65z/data=!5m1!1e1")
+  time.sleep(3)
+  #with open("page_source"+str(i)+".html", "w") as f:
+  #  f.write(driver.page_source)
+  
+  
+  driver.execute_script('document.getElementById("omnibox-container").remove()')
+  # driver.execute_script('document.getElementById("itamenu").remove()')
+  driver.execute_script('x = document.evaluate("/html/body/div[3]/div[9]/div[4]/div/div/div/div[1]/div/div/div/div",document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue')
+  driver.execute_script('x.remove()')
+  driver.execute_script('x = document.evaluate("//div[@data-ogsr-up]",document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue')
+  driver.execute_script('x.remove()')
+  driver.execute_script('x = document.evaluate("/html/body/div[3]/div[9]/div[22]",document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue')
+  driver.execute_script('x.remove()')
+  driver.execute_script('x = document.evaluate("/html/body/div[3]/div[9]/div[24]",document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue')
+  driver.execute_script('x.remove()')
+  driver.execute_script('x = document.evaluate("/html/body/div[3]/div[9]/div[1]/div[2]",document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue')
+  driver.execute_script('x.remove()')
+  #canvas = driver.find_element(By.XPATH,'//div[contains(@aria-label,"خريطة")]')
+  canvas = driver.find_element(By.XPATH,'/html/body/div[3]/div[9]/div[1]/div[3]')
+  # actions = ActionChains(driver)
+  # actions.click_and_hold(canvas)
+  # action.perform()
+  # time.sleep(1.5)
+  # time
+  # get the canvas as a PNG base64 string
+  # decode
+
+  # save to a file
+  path = os.getcwd()
+  path = path.replace('\\','/')
+  folder = '/'.join([path,folder])
+  if not (os.path.exists(folder)):
+    os.makedirs(folder)
 
 
 
+  with open(folder+r"/"+'-'.join([str(date.hour),str(date.minute),str(date.second)])+".png", 'wb') as f:
+      f.write(canvas.screenshot_as_png)
+  print(datetime.today())
+  if i != 4:
+      time.sleep(55)
 driver.quit()
 
