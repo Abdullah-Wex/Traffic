b92479d8fc84cceaf4b2cb3d0958c210b71d2643
Abdullah-Wex <86279127+Abdullah-Wex@users.noreply.github.com>
2022-10-14 18:41:43 +0300

    Mission Z

--- main.py
+++ main.py
@@ -14,15 +14,48 @@ from webdriver_manager.chrome import ChromeType
 
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-options = Options()
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+    
+driver = webdriver.Chrome(options = chrome_options)
+
+#options = Options()
 # options.add_argument("--window-size=1920,1200")
-options.headless = True
-    # options.add_argument("--headless")
-# driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
-# driver <- 9
-# driver = webdriver.Chrome("D:\Files\MyFiles\ChromeDriver\chromedriver1.exe",options=options,keep_alive=True)
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-driver = webdriver.Chrome(service=chrome_service,options=options)
+#options.headless = True
+#chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+#driver = webdriver.Chrome(service=chrome_service,options=options)
 # driver.quit()
 
 
